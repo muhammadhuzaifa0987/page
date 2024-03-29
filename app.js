af23dfc466c82c5df1fdad7d719b77836e6c222e
muhammadhuzaifa0987 <164562502+muhammadhuzaifa0987@users.noreply.github.com>
2024-03-30 01:14:11 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+var FullName= prompt('Enteryourfullname' );
+var UserName= "@"+FullName+FullName.length;
+console.log(UserName);
+
+
+var itemname="pen";
+var price=50;
+var item= "pen "
+console.log(typeof "item");
